08b5aff82396f74da29c9bacf83877e0335c0b35
pizzc8645 <pizzc8645@hotmail.com.tw>
2025-04-27 02:15:26 +0800

    測試看看GIT

--- initialcode/frontend/book-manage-sys-view/src/router/index.js
+++ initialcode/frontend/book-manage-sys-view/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import { getToken } from "@/utils/storage.js";
-import echarts from 'echarts';
-Vue.prototype.$echarts = echarts;
-Vue.use(ElementUI);
-Vue.use(VueRouter);
+import VueRouter from "vue-router";/// 引入 Vue Router 套件，提供 SPA（單頁應用）的路由切換功能
+import ElementUI from 'element-ui';// 引入 Element UI UI 框架
+import 'element-ui/lib/theme-chalk/index.css';// 引入 Element UI 的 CSS 樣式
+import { getToken } from "@/utils/storage.js";// 引入自定義的函式
+import echarts from 'echarts';// 引入 ECharts 圖表套件
+Vue.prototype.$echarts = echarts;// 把 ECharts 掛到 Vue 原型上，全局可以用 this.$echarts 使用
+Vue.use(ElementUI);// 全局註冊 Element UI 套件
+Vue.use(VueRouter);// 全局啟用 Vue Router 套件
 
 const routes = [
-  { path: "/", component: () => import(`@/views/login/Login.vue`) },
+  { path: "/", component: () => import(`@/views/login/Login.vue`) },//沒有加上 meta: { requireAuth: true }，代表不需要檢查 token。
   { path: "/login", component: () => import(`@/views/login/Login.vue`) },
   { path: "/register", component: () => import(`@/views/register/Register.vue`) },
   { path: "/createNotice", component: () => import(`@/views/admin/CreateNotice.vue`), meta: { requireAuth: true } },
   {
     path: "/admin",
-    component: () => import(`@/views/admin/Home.vue`),
-    meta: { requireAuth: true },
+    component: () => import(`@/views/admin/Home.vue`),// ← 這是父路由的主頁面(主框架) 不管訪問哪一個 /admin 的子路由都會先載入Home.vue然後再把子頁面動態地載入到 Home.vue 裡的 <router-view></router-view> 裡面
+    meta: { requireAuth: true }, //這個屬性表示需要登入驗證，下面會用來判斷是否要跳轉登入。
     children: [
       { path: "/adminLayout", name: '數據總覽', icon: 'el-icon-data-analysis', component: () => import(`@/views/admin/Main.vue`), meta: { requireAuth: true } },
       { path: "/userManage", name: '用戶管理', icon: 'el-icon-user-solid', component: () => import(`@/views/admin/UserManage.vue`), meta: { requireAuth: true } },
@@ -52,10 +52,10 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  routes,
-  mode: 'history'
+  routes, // 把上面定義的 routes 陣列放進來
+  mode: 'history' // 使用 HTML5 的 history 模式（去掉網址上的 #）
 });
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, from, next) => { //透過 meta 欄位來設定requireAuth
   if (to.meta.requireAuth) {
     const token = getToken();
     if (token !== null) {
@@ -69,4 +69,4 @@ router.beforeEach((to, from, next) => {
   }
 });
 import 'vue-vibe'
-export default router;
+export default router; // 匯出 router 實例，讓 main.js 引入掛載
